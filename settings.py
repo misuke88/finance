346e1501324a3da2c299a0e4fb11f0c0b8cc2040
Lucy Park <me@lucypark.kr>
2015-08-06 15:03:07 +0900

    Separate settings

---
+++ settings.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+TARGETPATH = '.'    # fill me with 8K-gz
--- test.py
+++ test.py
@@ -9,9 +9,8 @@ from nltk.corpus import stopwords
 from pprint import pprint
 
 
+from settings import TARGETPATH
 from utils import checkdir, file_read, get_today, get_version
-
-TARGETPATH = 'F:/finance/8K.tar/8K-gz'
 
 # def openfiles(TARGETPATH):
 
@@ -30,13 +29,13 @@ if __name__ == '__main__':
 
 		with gzip.open(path) as f:
 			d = f.read().split("<DOCUMENT>")
-		
-		d.remove('') # remove null list 
-		
+
+		d.remove('') # remove null list
+
 		for i in range(len(d)):
 			item = d[i].replace('.', '').lower().split("item:")
 			documents.append(item[-1])
-	
+
 	stop = set(stopwords.words('english'))
 	texts = [[word for word in document.lower().split() if word not in stop] for document in documents]
 	corpus = [dictionary.doc2bow(text) for text in texts]
@@ -53,6 +52,6 @@ if __name__ == '__main__':
 	# item = d[3].replace('.', '').lower().split("item:")
 	# term = re.findall(r'\w+',d[1])
 	# c = Counter(term).most_common(10)
-		
+
 	# c = {(a, b) for a, b in term.items() if b>1}
 	# print d[3], item[-1]
