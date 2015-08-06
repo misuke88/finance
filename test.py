48b3134ecd3aa8b32ba0b1f5148e72ebfb4f9594
Lucy Park <me@lucypark.kr>
2015-08-06 15:10:48 +0900

    Convert tabs to spaces

--- test.py
+++ test.py
@@ -15,43 +15,47 @@ from utils import checkdir, file_read, get_today, get_version
 # def openfiles(TARGETPATH):
 
 #     filenames = glob.glob('%s/*.gz' % TARGETPATH)
-# 	for filename in filenames:
+#     for filename in filenames:
 #         print filename
 #         docs = file_read(filename)
 
 
 if __name__ == '__main__':
 
-	filenames = glob.glob('%s/*.gz' % TARGETPATH)
-	documents = []
-	for filename in filenames:
-		path = '%s' %(filename)
+    filenames = glob.glob('%s/*.gz' % TARGETPATH)
+    documents = []
+    for i, filename in enumerate(filenames):
 
-		with gzip.open(path) as f:
-			d = f.read().split("<DOCUMENT>")
+        if i % 100==0:
+            print i
 
-		d.remove('') # remove null list
+        path = '%s' %(filename)
 
-		for i in range(len(d)):
-			item = d[i].replace('.', '').lower().split("item:")
-			documents.append(item[-1])
+        with gzip.open(path) as f:
+            d = f.read().split("<DOCUMENT>")
 
-	stop = set(stopwords.words('english'))
-	texts = [[word for word in document.lower().split() if word not in stop] for document in documents]
-	corpus = [dictionary.doc2bow(text) for text in texts]
-	# pprint(texts)
+        d.remove('') # remove null list
 
-	dictionary = corpora.Dictionary(texts)
-	dictionary.save('/tmp/finance.dict')
-	idList = dictionary.token2id
+        for i in range(len(d)):
+            item = d[i].replace('.', '').lower().split("item:")
+            documents.append(item[-1])
+
+    stop = set(stopwords.words('english'))
+    texts = [[word for word in document.lower().split() if word not in stop] for document in documents]
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    # pprint(texts)
+
+    dictionary = corpora.Dictionary(texts)
+    dictionary.save('/tmp/finance.dict')
+    idList = dictionary.token2id
 
 
-	print(dictionary)
-	# items = []
-	# # for i in range(len(d)):
-	# item = d[3].replace('.', '').lower().split("item:")
-	# term = re.findall(r'\w+',d[1])
-	# c = Counter(term).most_common(10)
+    print(dictionary)
+    # items = []
+    # # for i in range(len(d)):
+    # item = d[3].replace('.', '').lower().split("item:")
+    # term = re.findall(r'\w+',d[1])
+    # c = Counter(term).most_common(10)
 
-	# c = {(a, b) for a, b in term.items() if b>1}
-	# print d[3], item[-1]
+    # c = {(a, b) for a, b in term.items() if b>1}
+    # print d[3], item[-1]
