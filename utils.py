0f6c7b7e0b210b81923b3aa14392aef5989adaf7
Lucy Park <me@lucypark.kr>
2015-08-12 03:44:53 +0900

    Replace tabs with spaces

--- utils.py
+++ utils.py
@@ -7,17 +7,20 @@ import os
 
 get_today = lambda : datetime.now()
 
+
 def checkdir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def file_read(filename):
-	with gzip.open(filename) as f:
-		# d = f.read().replace('.', '')
-		# doc = d.split("<DOCUMENT>")
 
-		d = f.read().split("<DOCUMENT>")
-	return d
+def file_read(filename):
+    with gzip.open(filename) as f:
+        # d = f.read().replace('.', '')
+        # doc = d.split("<DOCUMENT>")
+
+        d = f.read().split("<DOCUMENT>")
+    return d
+
 
 def get_version():
     with open('version.cfg', 'r') as f:
