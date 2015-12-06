3f29d72f731658077fa699129ebee832991757ea
misuke <mskim@dm.snu.ac.kr>
2015-12-06 11:33:09 +0900

    Add the openfiles, preprocessing functions, and change filename

--- sentence_parsing.py
+++ sentence_parsing.py
@@ -10,9 +10,34 @@ from settings import DATA_DIR, DIR_8K, DIR_PRICE
 import utils
 
 from utils import checkdir, file_read, re_sub, get_version, get_datetime
-from modeling import openfiles, preprocessing
 
 TOTAL_INDEX = 'djia gspc ixic vix'.split() # dow jones. snp500, nasdaq, vol
+
+def openfiles(filename, arg):
+
+    data = pd.read_csv(filename, sep='\t', header = 0)
+    data = data.where((pd.notnull(data)), '')   # Replace np.nan with ''
+    if arg == 100: # X
+        value = pd.DataFrame(data)
+        value.index = data['id']
+    else: # y
+        columns = TOTAL_INDEX[arg]
+        value = pd.DataFrame(data[TOTAL_INDEX[arg]])
+        value.index = data['id']
+    return value
+
+def preprocessing(docs, y, arg):
+
+    code = TOTAL_INDEX[arg]
+    idx = y[y[code] != 'ERROR'].index.tolist()
+    X = docs.loc[idx]
+    y = y.loc[idx]
+    idx = y[y[code] != 'STAY'].index.tolist()
+    X = docs.loc[idx]
+    y = y.loc[idx]
+    print len(y)
+     
+    return X, y
 
 def parse_sentence(X, y):
 
@@ -50,16 +75,17 @@ def gather_data(X):
 
 if __name__ == '__main__':
 
-    filenameX = '%s/stock_X.txt' % DATA_DIR
-    filenameY = '%s/stock_Y.txt' % DATA_DIR
+    filenameX = '%s/stock_4company_X_7days.txt' % DATA_DIR
+    filenameY = '%s/stock_4company_Y_7days.txt' % DATA_DIR
+
 
     X = openfiles(filenameX, 100)
     y = openfiles(filenameY, 1) # arg = 1: SNP500
     X, y = preprocessing(X, y, arg=1)
 
     data_senti = parse_sentence(X, y)
-    output_pos = '%s/stockpos.txt' % DATA_DIR
-    output_neg = '%s/stockneg.txt' % DATA_DIR
+    output_pos = '%s/stock_4company_7days.pos' % DATA_DIR
+    output_neg = '%s/stock_4company_7days.neg' % DATA_DIR
     stock_pos, stock_neg = gather_data(data_senti)
 
     print len(stock_pos), len(stock_neg)
